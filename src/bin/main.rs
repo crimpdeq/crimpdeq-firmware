791d87cdbcddb8e348a590e8bdd567da5513de5d
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-01-17 17:20:22 +0100

    feat: Create embassy tasks

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,23 +2,28 @@
 #![no_main]
 
 use core::cell::RefCell;
+use critical_section::Mutex;
 
 use bleps::{
     async_attribute_server::AttributeServer,
     asynch::Ble,
-    // ad_structure::{
-    //     create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
-    // },
-    attribute_server::NotificationData,
+    att::Att,
+    attribute_server::{NotificationData, WorkResult},
     gatt,
 };
 use bytemuck::bytes_of;
 use embassy_executor::Spawner;
+use embassy_sync::{
+    blocking_mutex::raw::NoopRawMutex,
+    channel::{self, Channel, Receiver, Sender},
+};
+use embassy_time::{Duration, Timer};
 use esp_alloc as _;
 use esp_backtrace as _;
 use esp_hal::{
     clock::CpuClock,
     gpio::{Input, Pull},
+    peripheral,
     rng::Rng,
     time,
     timer::{systimer::SystemTimer, timg::TimerGroup},
@@ -29,13 +34,23 @@ use esp_wifi::{ble::controller::BleConnector, init, EspWifiController};
 
 use tindeq::progressor::{ControlOpCode, DataPoint, ResponseCode};
 
-// When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
+// // When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
 macro_rules! mk_static {
     ($t:ty,$val:expr) => {{
         static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
         #[deny(unused_attributes)]
         let x = STATIC_CELL.uninit().write(($val));
         x
+    }};
+}
+
+#[macro_export]
+macro_rules! make_static {
+    ($t:ty, $val:expr) => ($crate::make_static!($t, $val,));
+    ($t:ty, $val:expr, $(#[$m:meta])*) => {{
+        $(#[$m])*
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        STATIC_CELL.init_with(|| $val)
     }};
 }
 
@@ -80,8 +95,18 @@ const ADVERTISING_DATA: &[u8] = &[
     0,
 ];
 
+pub const MEASURE_COMMAND_CHANNEL_SIZE: usize = 50;
+pub type MeasurementsChannel = Channel<NoopRawMutex, ResponseCode, MEASURE_COMMAND_CHANNEL_SIZE>;
+pub type MeasurementsReceiver =
+    Receiver<'static, NoopRawMutex, ResponseCode, MEASURE_COMMAND_CHANNEL_SIZE>;
+pub type MeasurementsSender =
+    Sender<'static, NoopRawMutex, ResponseCode, MEASURE_COMMAND_CHANNEL_SIZE>;
+
+// HAVE A STATIC BOOL THAT ENABLES/DISABLES A TASK THAT READS THE WEIGTH
+static WEIGTH_TASK_ENABLED: Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
+
 #[esp_hal_embassy::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: Spawner) -> ! {
     let config = Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
@@ -108,12 +133,23 @@ async fn main(_spawner: Spawner) -> ! {
     let pin_ref = RefCell::new(button);
     let pin_ref = &pin_ref;
 
-    let mut bluetooth = peripherals.BT;
-    let connector = BleConnector::new(init, &mut bluetooth);
+    let connector = BleConnector::new(init, peripherals.BT);
+
+    let ch: &MeasurementsChannel = make_static!(MeasurementsChannel, Channel::new());
+    let rx: &MeasurementsReceiver = make_static!(MeasurementsReceiver, ch.receiver());
+
+    spawner.spawn(bt_task(connector, *rx)).unwrap();
+    spawner.spawn(measurement_task(ch)).unwrap();
+
+    loop {
+        Timer::after(Duration::from_millis(10)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn bt_task(connector: BleConnector<'static>, channel: MeasurementsReceiver) {
     let now = || time::now().duration_since_epoch().to_millis();
     let mut ble = Ble::new(connector, now);
-    println!("Connector created");
-
     loop {
         println!("ble.init: {:?}", ble.init().await);
         println!(
@@ -163,8 +199,15 @@ async fn main(_spawner: Spawner) -> ! {
                 }
                 ControlOpCode::StartMeasurement => {
                     println!("StartMeasurement");
+
+                    critical_section::with(|cs| {
+                        *WEIGTH_TASK_ENABLED.borrow_ref_mut(cs) = true;
+                    });
                 }
                 ControlOpCode::StopMeasurement => {
+                    critical_section::with(|cs| {
+                        *WEIGTH_TASK_ENABLED.borrow_ref_mut(cs) = false;
+                    });
                     println!("StopMeasurement");
                 }
                 ControlOpCode::GetAppVersion => {
@@ -262,68 +305,27 @@ async fn main(_spawner: Spawner) -> ! {
         ]);
 
         let mut rng = bleps::no_rng::NoRng;
-        let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes, &mut rng);
+        let mut server = AttributeServer::new(&mut ble, &mut gatt_attributes, &mut rng);
 
-        let counter = &RefCell::new(0u8);
-        let weigth = &RefCell::new(0.0f32);
-        let timestamp = &RefCell::new(0u32);
-        let mut notifier = || {
-            // TODO how to check if notifications are enabled for the characteristic?
-            // maybe pass something into the closure which just can query the characteristic
-            // value probably passing in the attribute server won't work?
-
-            async {
-                pin_ref.borrow_mut().wait_for_rising_edge().await;
-                {
-                    // Fake data
-                    let mut counter = (*counter).borrow_mut();
-                    *counter += 1;
-                    let mut weigth = (*weigth).borrow_mut();
-                    *weigth = *counter as f32 + (*counter as f32 / 10.0);
-                    let mut timestamp = (*timestamp).borrow_mut();
-                    *timestamp = time::now().duration_since_epoch().to_micros() as u32;
-                }
-                let measurement =
-                    ResponseCode::WeigthtMeasurement(*weigth.borrow(), *timestamp.borrow());
-                let data_point = DataPoint::new(measurement);
-                let data = bytes_of(&data_point);
-                NotificationData::new(data_point_handle, data)
-            }
+        let mut notifier = || async {
+            let measurement = channel.receive().await;
+            // let timestamp = (time::now().duration_since_epoch()).to_micros() as u32;
+            // let measurement = ResponseCode::WeigthtMeasurement(23.1f32, timestamp);
+            let data_point = DataPoint::new(measurement);
+            let data: &[u8] = bytes_of(&data_point);
+            NotificationData::new(data_point_handle, data)
         };
+        server.run(&mut notifier).await.unwrap();
 
-        srv.run(&mut notifier).await.unwrap();
-        // loop {
-        //     let mut notification = None;
+        // if let Ok(measurement) = channel.try_receive() {
+        //     println!("Received measurement");
+        //     // let timestamp = (time::now().duration_since_epoch()).to_micros() as u32;
+        //     // let measurement = ResponseCode::WeigthtMeasurement(23.1f32, timestamp);
+        //     let data_point = DataPoint::new(measurement);
+        //     let data: &[u8] = bytes_of(&data_point);
+        //     let notification = Some(NotificationData::new(data_point_handle, data));
 
-        //     if button.is_low() && debounce_cnt > 0 {
-        //         debounce_cnt -= 1;
-        //         if debounce_cnt == 0 {
-        //             // Fake data
-        //             let weigth = counter as f32 + (counter as f32 / 10.0);
-        //             let timestamp = time::now().duration_since_epoch().to_micros() as u32;
-        //             let measurement = ResponseCode::WeigthtMeasurement(weigth, timestamp);
-        //             let data_point = DataPoint::new(measurement);
-        //             let measurement_data = bytes_of(&data_point);
-        //             counter += 1;
-
-        //             let mut cccd = [0u8; 1];
-        //             if let Some(1) =
-        //                 srv.get_characteristic_value(data_point_notify_enable_handle, 0, &mut cccd)
-        //             {
-        //                 // if notifications enabled
-        //                 if cccd[0] == 1 {
-        //                     notification =
-        //                         Some(NotificationData::new(data_point_handle, measurement_data));
-        //                 }
-        //             }
-        //         }
-        //     };
-
-        //     if button.is_high() {
-        //         debounce_cnt = 500;
-        //     }
-
-        //     match srv.do_work_with_notification(notification) {
+        //     match server.do_work_with_notification(notification).await {
         //         Ok(res) => {
         //             if let WorkResult::GotDisconnected = res {
         //                 break;
@@ -334,5 +336,30 @@ async fn main(_spawner: Spawner) -> ! {
         //         }
         //     }
         // }
+
+        // Timer::after(Duration::from_millis(10)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn measurement_task(channel: &'static MeasurementsChannel) {
+    let mut counter = 0;
+    let mut weigth = 0.0f32;
+    loop {
+        let enabled = critical_section::with(|cs| *WEIGTH_TASK_ENABLED.borrow_ref(cs));
+
+        if enabled {
+            println!("Measuring weigth");
+            // TODO Measure the weigth
+            // Fake data
+            counter += 1;
+            weigth = counter as f32 / 10.0;
+            let timestamp = (time::now().duration_since_epoch()).to_micros() as u32;
+            let measurement = ResponseCode::WeigthtMeasurement(weigth, timestamp);
+
+            channel.send(measurement).await;
+        }
+        // Freq is 80Hz so ~13ms
+        Timer::after(Duration::from_millis(13)).await;
     }
 }
